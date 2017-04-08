55ea093cb9b132fb294ca91da0cd55729050c068
TeodorVecerdi <teo.vecerdi@gmail.com>
2017-04-08 04:00:48 +0300

    Commit #7
    Signed-off-by: TeodorVecerdi <teo.vecerdi@gmail.com>

--- src/main/java/com/powersoft/itec2017/alerts/AddLocationAlert.java
+++ src/main/java/com/powersoft/itec2017/alerts/AddLocationAlert.java
@@ -3,6 +3,7 @@ package com.powersoft.itec2017.alerts;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -38,22 +39,34 @@ public class AddLocationAlert {
         Button noButton = new Button("No");
         // TODO: 07.04.2017 verificare format data
         yesButton.setOnAction(e -> {
-            result = String.valueOf(location.getText()+";"+startDate.getText()+";"+endDate.getText()+";"+price.getText());
-            window.close();
+            String error = "";
+            if (!startDate.getText().matches("^\\d{1,2}\\/\\d{1,2}\\/\\d{4}$"))
+                error += "Start date format wrong\n";
+            if (!endDate.getText().matches("^\\d{1,2}\\/\\d{1,2}\\/\\d{4}$"))
+                error += "End date format wrong\n";
+            if (!error.isEmpty()) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(error);
+                alert.setContentText(null);
+                alert.showAndWait();
+            } else {
+                result = String.valueOf(location.getText() + ";" + startDate.getText() + ";" + endDate.getText() + ";" + price.getText());
+                window.close();
+            }
         });
 
-        noButton.setOnAction(e ->{
-            result= "";
+        noButton.setOnAction(e -> {
+            result = "";
             window.close();
         });
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(10));
-        HBox laba= new HBox();
+        HBox laba = new HBox();
         laba.setSpacing(10);
         laba.setAlignment(Pos.CENTER);
         laba.getChildren().addAll(yesButton, noButton);
-        layout.getChildren().addAll(location, startDate, endDate, price,laba);
+        layout.getChildren().addAll(location, startDate, endDate, price, laba);
         layout.setAlignment(Pos.CENTER);
 
         //Display window and wait for it to be closed before returning
